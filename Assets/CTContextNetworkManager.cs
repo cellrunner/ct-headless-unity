f3e839d651fa34be84612421c788e744795af466
daoismz <christian.solon@breederdao.io>
2025-02-13 14:25:15 +0800

    Downgraded to 4.26.0 from 4.29.0 to remain compatible with SmartContractCustomErrorRevertExceptionErrorABI.

--- Assets/CTContextNetworkManager.cs
+++ Assets/CTContextNetworkManager.cs
@@ -1,4 +1,5 @@
 using CTHeadless;
+using System.Threading.Tasks;
 using UnityEngine;
 
 public class CTContextNetworkManager : MonoBehaviour
@@ -11,10 +12,25 @@ public class CTContextNetworkManager : MonoBehaviour
     [Header("Sequencer")]
     [SerializeField] public string ctSequencerURl;
 
+    void Awake()
+    {
+        CTCommon.Logger.LogFn = (string msg, CTCommon.Logger.LogLevel level) =>
+        {
+            Debug.Log(msg);
+        };
+    }
+
     // Start is called before the first frame update
     void Start()
     {
+        FindMatch().Wait(5000);
+    }
+    async Task FindMatch()
+    {
         CTServerContext ctContext = new CTServerContext(url, privateKey, worldAddress, ctSequencerURl);
+
+        await ctContext.FindMatch();
+        Debug.Log("Found mach!");
     }
 
     // Update is called once per frame
